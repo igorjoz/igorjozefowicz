0d2c11c7442e561cde70c64c81d8702888eaa3b2
Igor Jozefowicz <igorjozefowicz@gmail.com>
2023-11-27 21:44:18 +0100

    develop home.index view & styles

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -12,7 +12,36 @@
     <h2 class="home__subheader">
         Software Engineer
     </h2>
+
+    {{-- links wrapper --}}
+    <div class="home__links-wrapper">
+        <a href="https://github.com/igorjoz"
+            class="home__link"
+            target="_blank"
+            rel="noopener noreferrer">
+            Github
+            {{-- <i class="fab fa-github"></i> --}}
+        </a>
+
+        <a href="https://www.linkedin.com/in/igor-jozefowicz/"
+            class="home__link"
+            target="_blank"
+            rel="noopener noreferrer">
+            LinkedIn
+        </a>
+
+        {{-- email --}}
+        <a href="mailto:igor@jozefowicz.pl"
+            class="home__link"
+            target="_blank"
+            rel="noopener noreferrer">
+            Email
+        </a>
+    </div>
 </div>
+
+{{-- link to github --}}
+
 
 {{-- @include('components.main.footer') --}}
 

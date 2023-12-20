fa95b1d9e85ea660a286bfef6ec476739bf90799
Igor Jozefowicz <igorjozefowicz@gmail.com>
2023-12-20 23:59:27 +0100

    develop work.giganci_programowania page

--- resources/views/work/giganci_programowania.blade.php
+++ resources/views/work/giganci_programowania.blade.php
@@ -6,29 +6,52 @@
 
 <div class="home__header-wrapper">
     <h1 class="home__header-name">
-        App Inventor 06.12.2023
+        🐍 <span class="pulsate">Python z Minecraftem</span>  20.12.2023
     </h1>
 
-    <h2 class="home__subheader">
-        Quiz Game część 1., kliknij link poniżej
-    </h2>
+    {{-- <h2 class="home__subheader">
+        Quiz #3 z AppInventora, kliknij link poniżej
+    </h2> --}}
 
     <div class="home__links-wrapper">
-        {{-- <a href="https://drive.google.com/file/d/1M_HD4pG6krmgymNY6SXDZSHFJtiamRWi/view?usp=sharing"
-            class="home__link"
-            target="_blank"
-            rel="noopener noreferrer">
-            Projekt pek_kluczy_1.aia
-        </a> --}}
-
-        {{-- link to public storage - gp directory and pek_kluczy_1.aia file --}}
-        <a href="{{ Storage::url('public/gp/Quiz_Game_starter.aia') }}"
-            class="home__link"
-            target="_blank"
-            rel="noopener noreferrer">
-            Plik Quiz_Game_starter.aia
+        {{-- <a href="{{ Storage::url('public/gp/Quiz_Game_starter.aia') }}" --}}
+        <a href="https://quiz.giganciprogramowania.edu.pl/pigk2-q3" class="home__link home__link--big" target="_blank" rel="noopener noreferrer">
+            📱AppInventor - Quiz #3
         </a>
     </div>
+
+    <div class="home__links-wrapper">
+        <a href="https://onlinegdb.com/fPj18yRpi" class="home__link home__link--big" target="_blank" rel="noopener noreferrer">
+            🐲Python - podstawy
+        </a>
+    </div>
+
+    {{-- <div class="home__links-wrapper">
+        <a href="https://onlinegdb.com/FTg1vUSK3" class="home__link home__link--big" target="_blank" rel="noopener noreferrer">
+            🐛Python - bug search #1
+        </a>
+    </div> --}}
+
+    {{-- <div class="home__links-wrapper">
+        <a href="https://onlinegdb.com/g2_TUV9ZM" class="home__link home__link--big" target="_blank" rel="noopener noreferrer">
+            👾Python - bug search #2
+        </a>
+    </div> --}}
+
+    {{-- <div class="home__links-wrapper">
+        <a href="https://drive.google.com/file/d/1bW9SRfwzItro_A1sF--GAqD3K55eDVsd/view?usp=drive_link" class="home__link home__link--big" target="_blank" rel="noopener noreferrer">
+            ⛏Minecraft - koordynaty
+        </a>
+    </div> --}}
+
+    {{-- PROJEKT ZOMIBIEZZ --}}
+    {{-- <div class="home__links-wrapper">
+        <h2 class="home__subheader">
+            🚀Minecraft - projekt - skopiuj link
+        <br>
+        https://makecode.com/_VhEex8WjkFKh
+        <h2 class="home__subheader">
+    </div> --}}
 </div>
 
 {{-- @include('components.main.footer') --}}

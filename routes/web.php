63ade8ac4e7229cca8292956010d2fc7e6acb01b
Igor Józefowicz <igorjozefowicz@gmail.com>
2024-02-09 22:27:05 +0100

    create /services route
    - add additional routes for /gp & /about-me

--- routes/web.php
+++ routes/web.php
@@ -25,15 +25,24 @@ Route::get('/home-page', [HomeController::class, 'index']);
 Route::get('/', [HomeController::class, 'index'])
     ->name('home.index');
 
+Route::get('/bio', [HomeController::class, 'aboutMe']);
+Route::get('/biography', [HomeController::class, 'aboutMe']);
 Route::get('/about-me', [HomeController::class, 'aboutMe'])
     ->name('home.about_me');
 
 Route::get('/contact', [HomeController::class, 'contact'])
     ->name('home.contact');
 
+Route::get('/usługi', [HomeController::class, 'services']);
+Route::get('/uslugi', [HomeController::class, 'services']);
+Route::get('/services', [HomeController::class, 'services'])
+    ->name('home.services');
 
 Route::get('/blog', [BlogController::class, 'index'])
     ->name('blog.index');
 
+Route::get('/giganci-programowania', [WorkController::class, "giganciProgramowania"]);
+Route::get('/giganci', [WorkController::class, "giganciProgramowania"]);
+Route::get('/g', [WorkController::class, "giganciProgramowania"]);
 Route::get('/gp', [WorkController::class, "giganciProgramowania"])
     ->name('work.giganci_programowania');

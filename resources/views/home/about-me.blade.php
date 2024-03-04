4b0902eee34e6e645904ce9d9edc0afdbe16b1ce
Igor Józefowicz <igorjozefowicz@gmail.com>
2024-03-04 13:03:56 +0100

    develop home views & styles
    - add photo of me to public storage

--- resources/views/home/about-me.blade.php
+++ resources/views/home/about-me.blade.php
@@ -2,6 +2,18 @@
 
 @section('content')
 
-about me page
+<section class="home__header-section">
+    <h1 class="home__header-name about__header">
+        About me
+    </h1>
+
+    <div class="home__header-image-wrapper">
+        <img src="{{ Storage::url('public/home/Igor.jpg') }}" alt="Igor Józefowicz" class="about__photo">
+    </div>
+
+    <h2 class="home__subheader-role about__subheader">
+        Igor Józefowicz
+    </h2>
+</section>
 
 @endsection
--- resources/views/home/services.blade.php
+++ resources/views/home/services.blade.php
@@ -2,16 +2,10 @@
 
 @section('content')
 
-{{-- @include('components.main.navigation') --}}
-
 <section class="home__header-section">
     <h1 class="home__header-name">
         Need a website?
     </h1>
-
-    {{-- <div class="home__header-image-wrapper">
-        <img src="{{ Storage::url('public/favicon.png') }}" alt="Igor Józefowicz" class="home__header-image">
-    </div> --}}
 
     <h2 class="home__subheader-role">
         We will make it happen!
@@ -52,27 +46,5 @@
     {{-- scroll arrow --}}
 
 </section>
-
-<section class="home__bio-section">
-    <div class="home__bio-text-wrapper">
-        <h2 class="home__bio-header">
-            About me
-        </h2>
-
-        <p class="home__bio-text">
-            Hello there! I'm Igor Józefowicz, also known as <a href="https://github.com/igorjoz" target="_blank" rel="noopener noreferrer" class="home__link">&commat;igorjoz</a>. Since 2018, I've&nbsp;been immersed in web development, focusing on PHP & Laravel to&nbsp;create impactful digital solutions. I&nbsp;complement my technical endeavors with academic studies in Computer Science at Gdańsk University of Technology.
-        </p>
-
-        <p class="home__bio-text">
-            As a&nbsp;passionate educator, I&nbsp;teach programming at Giganci Programowania, covering topics from Hacking & Cybersecurity to Python and C++. When not teaching, I&nbsp;enjoy challenging myself with Competitive Programming contests and deepening my&nbsp;understanding of Computer Architecture & Assembly, alongside Database Design.
-        </p>
-
-        <p class="home__bio-text">
-            I'm excited to connect with fellow tech enthusiasts and professionals. Explore my website to learn more about my work or reach out on <a href="https://www.linkedin.com/in/igor-jozefowicz/" target="_blank" rel="noopener noreferrer" class="home__link">LinkedIn</a>. For collaborations or inquiries, don't hesitate to send me an email: <a href="mailto:igor@jozefowicz.pl" target="_blank" rel="noopener noreferrer" class="home__link">igor@jozefowicz.pl</a>.
-        </p>
-    </div>
-</section>
-
-@include('components.main.footer')
 
 @endsection

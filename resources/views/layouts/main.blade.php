ee62eaec89dad40ffe2f6b5a93ae3ac72b715c28
Igor Józefowicz <igorjozefowicz@gmail.com>
2024-03-06 07:11:15 +0100

    add Google Tag to layouts.main

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,6 +2,15 @@
 <html lang="en">
 
 <head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LMN39KC4VT"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-LMN39KC4VT');
+    </script>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
@@ -25,7 +34,7 @@
 </head>
 
 <body>
-    @include('components.main.navigation')
+    {{-- @include('components.main.navigation') --}}
 
     @yield('content')
 

40e7efb2b9c6606fe3d2448ce3a66a3ea7205b86
Igor Józefowicz <igorjozefowicz@gmail.com>
2025-10-16 23:26:03 +0200

    Add ghost arrow cascade animation to ScrollArrow
    Introduces ghost arrow elements and associated CSS animations for a cascading shooting effect on hover in the ScrollArrow component. Refactors markup and styles to support layered arrow visuals and improves tactile feedback.

--- resources/js/components/ScrollArrow.jsx
+++ resources/js/components/ScrollArrow.jsx
@@ -9,7 +9,14 @@ export default function ScrollArrow({ targetId }) {
                 className="scroll-arrow text-white/90 hover:text-[var(--color-primary)] transition-colors text-5xl sm:text-6xl md:text-7xl drop-shadow-[0_0_10px_rgba(255,107,203,0.35)]"
                 style={{ lineHeight: 1 }}
             >
-                <i className="fa fa-chevron-down" aria-hidden="true"></i>
+                <span className="arrow-stack" aria-hidden="true">
+                    {/* Main arrow */}
+                    <i className="fa fa-chevron-down arrow-main" aria-hidden="true"></i>
+                    {/* Ghost arrows that cascade on hover */}
+                    <i className="fa fa-chevron-down arrow-ghost ghost-1" aria-hidden="true"></i>
+                    <i className="fa fa-chevron-down arrow-ghost ghost-2" aria-hidden="true"></i>
+                </span>
+                <span className="sr-only">Scroll down</span>
             </a>
         </div>
     );

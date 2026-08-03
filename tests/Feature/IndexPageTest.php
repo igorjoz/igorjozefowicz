2823058e14dc12edb2bdeafaab4bf1a5e76344e1
Igor Józefowicz <igorjozefowicz@gmail.com>
2026-08-04 00:13:17 +0200

    Replace unit test with feature test for index page loading

---
+++ tests/Feature/IndexPageTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Inertia\Testing\AssertableInertia as Assert;
+use Tests\TestCase;
+
+class IndexPageTest extends TestCase
+{
+    public function test_index_page_loads_correctly(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertOk();
+        $response->assertInertia(
+            fn (Assert $page) => $page->component('Home/Index')
+        );
+    }
+}
